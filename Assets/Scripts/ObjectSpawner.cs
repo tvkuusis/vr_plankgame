d7fe8e3197e7d202312f3a19b7ab255b45e7c89d
Tuomo Kuusisto <kuusisto.tuomo@gmail.com>
2017-11-24 13:27:35 +0200

    wip

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class ObjectSpawner : MonoBehaviour {
 
     public GameObject spawnedObject;
-    BoxCollider exitArea;
+    public float destroyTimer;
+
+    BoxCollider exitArea;   // Gameobject is destroyed if it leaves exitArea
 
 	void Start () {
-		
-	}
+        exitArea = GetComponent<BoxCollider>();
+        transform.GetChild(0).gameObject.SetActive(false);        // Set visualiser off
+    }
 
 	void Update () {
 		
 	}
 
+    void OnTriggerExit(Collider col){
+        if(col.gameObject == spawnedObject) {
+            Destroy(col, destroyTimer);
+        }
+    }
 
+    void SpawnObject(){
+
+    }
 }
